9cbf573ec78f773556c997d71ef3339adc81eca6
Matias Farias <mat.fariasc@icloud.com>
2021-09-29 20:58:25 -0300

    "pruebas del modulo noticas y boton para la busqueda"

--- apps/noticias/twitter.py
+++ apps/noticias/twitter.py
@@ -20,19 +20,22 @@ def obtenerTwitters(keywords,cant):
 
     search_words = words
 
-    tweets = api.search_tweets(q=search_words,lang="es",result_type="recent",count=cant)
+    tweets = api.search_tweets(q=keywords,lang="es",result_type="mixed",count=cant)
     return tweets
     # for tweet in tweets:
     #     print(f"created_at: {tweet.created_at}\nuser: {tweet.user.screen_name}\ntweet text: {tweet.text}\ngeo_location: {tweet.user.location}\nurl: https://twitter.com/twitter/statuses/{tweet.id}")
     #     print("\n")
 
 def GrabarTwitters(tweets,company):
-    medio = Medio.objects.filter(nombre__contains="twitter")
+    medio = Medio.objects.get(id=1)
     for tweet in tweets:
-        Noticia.objects.create(
-            contenido = tweet.text,
-            url = tweet.id,
-            date = tweet.created_at,
-            medio = medio,
-            company = company
-            )
+        try:
+            Noticia.objects.create(
+                contenido = tweet.text,
+                url = tweet.id,
+                date = tweet.created_at,
+                medio = medio,
+                company = company
+                )
+        except:
+            print("Ya existe esta nota")
