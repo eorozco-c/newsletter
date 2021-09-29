7335ed26d75569fea8623663521d279ff947efd0
Ernesto Orozco C <ernestorozco94@gmail.com>
2021-09-29 12:49:34 -0300

    Se agregó pestaña Medios los cuales se crean por compañias

--- apps/medios/formularios.py
+++ apps/medios/formularios.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import TipoMedio
+from .models import TipoMedio, Medio
 
 class FormularioTipoMedio(forms.ModelForm):
 
     class Meta:
         model = TipoMedio
         fields = ["nombre"]
+
+class FormularioMedio(forms.ModelForm):
+
+    class Meta:
+        model = Medio
+        fields = ["nombre", "tipo"]
